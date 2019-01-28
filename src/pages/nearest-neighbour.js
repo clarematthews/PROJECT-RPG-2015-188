877f395a08ae416593669dbb2e4ce16a743c99db
Clare Matthews <clarematthews@gmail.com>
2019-01-28 15:06:37 +0000

    Add nearest-neighbour video

--- src/pages/nearest-neighbour.js
+++ src/pages/nearest-neighbour.js
@@ -14,7 +14,7 @@ const NearestNeighbourPage = () => (
     </div>
     <hr />
     <div style={{ position: 'relative', paddingBottom: '56.25%', paddingTop: '25px', height: '0' }}>
-	    Video
+	    <iframe style={{ position: 'absolute', top: '0', left: '0', width: '100%', height: '100%' }} width="560" height="315" src="https://www.youtube.com/embed/08JABhcErAU?vq=hd720&modestbranding=1&controls=0&amp;" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
     </div>
     <hr />
     <div style={{paddingBottom:'25px'}}>
